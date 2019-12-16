b87c101008dad2a64d505f220739a8dea6dc7967
Bryan Lopez <bryan.lopez@berkeley.edu>
2019-12-15 18:19:57 -0800

    Completed table; todo: create filtering

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,27 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Locate Table, id="#ufo-table"
+// Locate Body
+
+var table = d3.select("#ufo-table");
+var tbody = table.select("tbody")
+
+// Attach data to node
+
+var tr = tbody.selectAll("tr")
+  .data(data)
+  .enter()
+  .append("tr");
+
+// Unwrap object data
+// keys:
+//   datetime, city, state, country, shape, durationMinutes, comments
+
+var td = tr.selectAll("td")
+  .data( function(d) {
+    return Object.values(d);
+  })
+  .enter()
+  .append("td")
+  .text(d => d); //Add data to td tag 
